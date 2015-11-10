0055a371133e1457c7dde6de9153c14748fb7ded
Brian Juul Andersen <brianmanden@gmail.com>
2015-11-10 15:36:24 +0100

    Starting coding TcpListener

--- WindowsService1/Service1.cs
+++ WindowsService1/Service1.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WindowsService1
 {
     public partial class Service1 : ServiceBase
     {
+
         public Service1()
         {
             InitializeComponent();
@@ -24,12 +20,50 @@ namespace WindowsService1
 
         protected override void OnStart(string[] args)
         {
-            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart123.txt");
+            Listen();
         }
 
         protected override void OnStop()
         {
             System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStop.txt");
+        }
+
+        private void Listen()
+        {
+            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "ListenStarted.txt");
+            /*
+            int port = 49152;
+
+            var listener = new TcpListener(IPAddress.Parse(LocalIPAddress()), port);
+            listener.Start();
+            */
+        }
+
+        private string LocalIPAddress()
+        {
+            string localIP = String.Empty;
+            try
+            {
+                IPHostEntry host;
+                host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (IPAddress ip in host.AddressList)
+                {
+                    if (ip.AddressFamily.ToString() == "InterNetwork")
+                    {
+                        localIP = ip.ToString();
+                    }
+                }
+                Console.WriteLine("--->" + localIP);
+                return localIP;
+            }
+            catch (Exception ex)
+            {
+                //LogWriter.WriteExceptionLog(ex.ToString());
+                Console.WriteLine(ex.ToString());
+            }
+
+            return localIP;
         }
     }
 }
